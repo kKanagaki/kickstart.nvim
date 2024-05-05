e0545a3725f1d0aca07e2a1a34dce9f3d889922d
koji.kanagaki <kanagaki@hotelovers.co.jp>
2024-05-05 15:42:07 +0900

    update: ステータスライン変更用にlualineを追加

--- init.lua
+++ init.lua
@@ -890,6 +890,11 @@ require('lazy').setup({
     end,
   },
 
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+  },
+
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
